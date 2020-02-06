358387014da13b5d4236ede677056e54592f8312
Samuel Escobar <sescobar8714@gmail.com>
2020-02-06 21:52:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,9 +17,9 @@ def directors_totals(nds)
 
     while movie_counter < director_movies.length do
       current_movie = director_movies[movie_counter]
-      
+
       director_sum += current_movie[:worldwide_gross]
-      
+
       puts "Director movie count: #{movie_counter}"
       puts "Movie Sum: #{current_movie[:worldwide_gross]}"
       puts "---------------"
